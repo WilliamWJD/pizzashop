10e6ff84ce9e6b5d444ad3c183f5a0f081365bc2
WilliamWJD <william.jose.wjd@gmail.com>
2024-01-21 10:50:55 -0300

    feat: criando pagina sign-up

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -5,6 +5,7 @@ import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 import { toast } from 'sonner'
+import { Link } from 'react-router-dom'
 
 const signInForm = z.object({
   email: z.string().email(),
@@ -41,7 +42,11 @@ export function SignIn() {
     <>
       <Helmet title="login" />
       <div className="p-8">
-        <div className="W-[250px] flex flex-col justify-center gap-6">
+        <Button asChild className="absolute right-8 top-8" variant="ghost">
+          <Link to="/sign-up">Novo estabelecimento</Link>
+        </Button>
+
+        <div className="w-[350px] flex flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="tex-2xl font-semibold tracking-tight">
               Acessar painel
